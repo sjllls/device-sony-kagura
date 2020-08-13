APokorny/device-sony-dora

Add cm makefile

cm.mk
@@ -1,0 +1,1 @@
+$(call inherit-product, device/sony/dora/aosp_f8131.mk)

sjllls/device-sony-kagura

cm.mk
@@ -1,0 +1,1 @@
+$(call inherit-product, device/sony/kagura/aosp_f8331.mk)